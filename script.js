f0635bc07d746b67c6f468cbb0b64d2cd9548a99
Aslam Khan shah <aksi34012@gmail.com>
2023-04-06 00:57:10 +0530

    wrote the code

---
+++ script.js
@@ -0,0 +1,23 @@
+// Time interval in milliseconds after which the history, cookies and cache will be cleared
+const intervalInMilliseconds = 24 * 60 * 60 * 1000; // 24 hours
+
+// Function to clear browsing history, cookies, and cache
+function clearData() {
+  // Clear browsing history
+  window.history.go(-(window.history.length));
+  
+  // Clear cookies
+  document.cookie.split(";").forEach(function(c) {
+    document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+  });
+  
+  // Clear cache
+  window.caches.keys().then(function(cacheNames) {
+    cacheNames.forEach(function(cacheName) {
+      window.caches.delete(cacheName);
+    });
+  });
+}
+
+// Set a timer to clear browsing history, cookies, and cache
+setTimeout(clearData, intervalInMilliseconds);
